88647400eaf7baef08859fe70ea680b492fdea03
Pitt Madison <karimovpg@gmail.com>
2019-06-09 04:26:36 +0300

    second dice added, some fixes

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var scores, roundScore, activePlayer, gamePlaying, sixCounter, winScore;
+var scores, roundScore, activePlayer, gamePlaying, sixCounter, secondSixCounter, winScore;
 
 init();
 
@@ -8,20 +8,28 @@ document.querySelector('.score__input').addEventListener('input', getInputValue)
 document.querySelector('.btn-roll').addEventListener('click', function () {
 
     if (gamePlaying) {
+
         var dice = Math.floor(Math.random() * 6) + 1;
+        var secondDice = Math.floor(Math.random() * 6) + 1;
 
         dice === 6 ? sixCounter ++ : sixCounter = 0;
+        secondDice === 6 ? secondSixCounter ++ : secondSixCounter = 0;
+
         
         var diceDOM = document.querySelector('.dice');
         diceDOM.src = './img/dice-' + dice + '.png';
         diceDOM.style.display = 'block';
 
-        if (dice !== 1 && sixCounter !== 2) {
+        var secondDiceDOM = document.querySelector('.dice--mirrored');
+        secondDiceDOM.src = './img/dice-' + secondDice + '.png';
+        secondDiceDOM.style.display = 'block';
+
+        if (dice !== 1 && secondDice !== 1 && sixCounter !== 2 && secondSixCounter !== 2 ) {
    
-            roundScore += dice;
+            roundScore += dice + secondDice;
             document.querySelector('#current-' + activePlayer).textContent = roundScore;
 
-        } else  if (sixCounter === 2) {
+        } else  if (sixCounter === 2 || secondSixCounter === 2) {
 
             document.getElementById('score-' + activePlayer).textContent = '0';
             scores[activePlayer] = 0;
@@ -45,6 +53,7 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
 
             document.getElementById('name-' + activePlayer).textContent = 'Winner !!!';
             document.querySelector('.dice').style.display = 'none';
+            document.querySelector('.dice--mirrored').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
 
             activePlayer === 0 ? document.querySelector('.player-0-panel').classList.remove('active') :
@@ -66,6 +75,7 @@ function nextPlayer() {
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
     sixCounter = 0;
+    secondSixCounter = 0;
 
     document.getElementById('current-0').textContent = '0';
     document.getElementById('current-1').textContent = '0';
@@ -74,6 +84,8 @@ function nextPlayer() {
     document.querySelector('.player-1-panel').classList.toggle('active-2');
 
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice--mirrored').style.display = 'none';
+
 }
 
 function init() {
@@ -82,9 +94,10 @@ function init() {
     activePlayer = 0;
     roundScore = 0;
     sixCounter = 0;
+    secondSixCounter = 0;
 
     document.querySelector('.score').reset();
-    
+
     document.getElementById('scoreInput').removeAttribute('disabled');
 
 
@@ -98,6 +111,8 @@ function init() {
     document.querySelector('.player-0-panel').classList.add('active');
 
     document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.dice--mirrored').style.display = 'none';
+
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
     document.getElementById('current-0').textContent = '0';
